2769138c7ca704d2f459073bed7e8ca19f2b3472
PierreOlivierFortin <forp1303@usherbrooke.ca>
2026-01-26 17:29:39 -0500

    Cristaux_tests et modifs cristaux.pl

--- Prolog/Cristaux.pl
+++ Prolog/Cristaux.pl
@@ -14,22 +14,33 @@ cristaux4([C1,C2,C3,C4], [C1,C2,C3,C4]).
 cristaux5([C1,C2,C3,C4,C5], [C1,C2,C3,C4,C5]).
 cristaux6([C1,C2,C3,C4,C5,C6], [C1,C2,C3,C4,C5,C6]).
 
+enlever_position([S,C1,C2,C3], P) :-
+    enlever_position3([S,C1,C2,C3], P).
+
+enlever_position([S,C1,C2,C3,C4], P) :-
+    enlever_position4([S,C1,C2,C3,C4], P).
+
+enlever_position([S,C1,C2,C3,C4,C5], P) :-
+    enlever_position5([S,C1,C2,C3,C4,C5], P).
+
+enlever_position([S,C1,C2,C3,C4,C5,C6], P) :-
+    enlever_position6([S,C1,C2,C3,C4,C5,C6], P).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 3 cristaux
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-enlever_position([_Serrure|Queue], Position) :-
+enlever_position3([_Serrure|Queue], Position) :-
     cristaux3(Queue, [C1,C2,C3]),
     (
-        \+ contient("red", [C1,C2,C3]) ->
+        \+ contient(red, [C1,C2,C3]) ->
             Position = second
-    ;   C3 = "white" ->
+    ;   C3 = white ->
             Position = third
-    ;   compte_couleur("blue", [C1,C2,C3], NbBleu),
+    ;   compte_couleur(blue, [C1,C2,C3], NbBleu),
         NbBleu > 1 ->
-            ( C3 = "blue" -> Position = third
-            ; C2 = "blue" -> Position = second
+            ( C3 = blue -> Position = third
+            ; C2 = blue -> Position = second
             ;               Position = first
             )
     ;   Position = first
@@ -39,26 +50,26 @@ enlever_position([_Serrure|Queue], Position) :-
 % 4 cristaux
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-enlever_position([Serrure|Queue], Position) :-
+enlever_position4([Serrure|Queue], Position) :-
     cristaux4(Queue, [C1,C2,C3,C4]),
     (
-        Serrure = "silver",
-        compte_couleur("red", [C1,C2,C3,C4], NbRouge),
+        Serrure = silver,
+        compte_couleur(red, [C1,C2,C3,C4], NbRouge),
         NbRouge > 1 ->
-            ( C4 = "red" -> Position = fourth
-            ; C3 = "red" -> Position = third
-            ; C2 = "red" -> Position = second
+            ( C4 = red -> Position = fourth
+            ; C3 = red -> Position = third
+            ; C2 = red -> Position = second
             )
 
-    ;   C4 = "yellow",
-        \+ contient("red", [C1,C2,C3,C4]) ->
+    ;   C4 = yellow,
+        \+ contient(red, [C1,C2,C3,C4]) ->
             Position = first
 
-    ;   compte_couleur("blue", [C1,C2,C3,C4], NbBleu),
+    ;   compte_couleur(blue, [C1,C2,C3,C4], NbBleu),
         NbBleu =:= 1 ->
             Position = first
 
-    ;   compte_couleur("yellow", [C1,C2,C3,C4], NbJaune),
+    ;   compte_couleur(yellow, [C1,C2,C3,C4], NbJaune),
         NbJaune > 1 ->
             Position = fourth
 
@@ -69,20 +80,20 @@ enlever_position([Serrure|Queue], Position) :-
 % 5 cristaux
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-enlever_position([Serrure|Queue], Position) :-
+enlever_position5([Serrure|Queue], Position) :-
     cristaux5(Queue, [C1,C2,C3,C4,C5]),
     (
-        C5 = "black",
-        Serrure = "gold" ->
+        C5 = black,
+        Serrure = gold ->
             Position = fourth
 
-    ;   compte_couleur("red", [C1,C2,C3,C4,C5], NbRouge),
+    ;   compte_couleur(red, [C1,C2,C3,C4,C5], NbRouge),
         NbRouge =:= 1,
-        compte_couleur("yellow", [C1,C2,C3,C4,C5], NbJaune),
+        compte_couleur(yellow, [C1,C2,C3,C4,C5], NbJaune),
         NbJaune > 1 ->
             Position = first
 
-    ;   compte_couleur("black", [C1,C2,C3,C4,C5], NbNoir),
+    ;   compte_couleur(black, [C1,C2,C3,C4,C5], NbNoir),
         NbNoir =:= 0 ->
             Position = second
 
@@ -93,21 +104,21 @@ enlever_position([Serrure|Queue], Position) :-
 % 6 cristaux
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-enlever_position([Serrure|Queue], Position) :-
+enlever_position6([Serrure|Queue], Position) :-
     cristaux6(Queue, [C1,C2,C3,C4,C5,C6]),
     (
-        compte_couleur("yellow", [C1,C2,C3,C4,C5,C6], NbJaune),
+        compte_couleur(yellow, [C1,C2,C3,C4,C5,C6], NbJaune),
         NbJaune =:= 0,
-        Serrure = "bronze" ->
+        Serrure = bronze ->
             Position = third
 
-    ;   compte_couleur("yellow", [C1,C2,C3,C4,C5,C6], NbJaune2),
+    ;   compte_couleur(yellow, [C1,C2,C3,C4,C5,C6], NbJaune2),
         NbJaune2 =:= 1,
-        compte_couleur("white", [C1,C2,C3,C4,C5,C6], NbBlanc),
+        compte_couleur(white, [C1,C2,C3,C4,C5,C6], NbBlanc),
         NbBlanc > 1 ->
             Position = fourth
 
-    ;   compte_couleur("red", [C1,C2,C3,C4,C5,C6], NbRouge),
+    ;   compte_couleur(red, [C1,C2,C3,C4,C5,C6], NbRouge),
         NbRouge =:= 0 ->
             Position = sixth
 
